430eb9c1b22ceb589865b25a305b6296d6e11c90
Aggelos <aggelos.almouti@gmail.com>
2025-03-03 23:51:07 +0200

    add css styling and working addTask function

---
+++ src/ToDoList.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState(["eat", "walk", "train", "code"]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {}
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+        <ol>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span className="text">{task}</span>
+              <button
+                className="delete-button"
+                onClick={() => deleteTask(index)}
+              >
+                🗑️
+              </button>
+              <button className="move-button" onClick={() => moveTaskUp(index)}>
+                ⬆️
+              </button>
+              <button
+                className="move-button"
+                onClick={() => moveTaskDown(index)}
+              >
+                ⬇️
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default ToDoList;
